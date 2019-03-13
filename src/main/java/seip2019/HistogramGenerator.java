573059bdd1ec11a879368eabf83e149948c01fc7
Stavros Karagiannidis <32717234+Stavros1997@users.noreply.github.com>
2019-03-13 09:22:06 +0200

    adding frequency array

--- src/main/java/seip2019/HistogramGenerator.java
+++ src/main/java/seip2019/HistogramGenerator.java
@@ -29,50 +29,69 @@ public class HistogramGenerator {
 			token1 = scn.next();
 			temps.add(token1);
 		}
-	}
+		int[] fr = new int[temps.size()];
+		int visited = -1;
 
-	
-			public void generateChart(int[] dataValues) {
-				/*
-				 * The XYSeriesCollection object is a set XYSeries series (dataset) that can be
-				 * visualized in the same chart
-				 */
-				XYSeriesCollection dataset = new XYSeriesCollection();
-				/*
-				 * The XYSeries that are loaded in the dataset. There might be many series in
-				 * one dataset.
-				 */
-				XYSeries data = new XYSeries("random values");
-
-				/*
-				 * Populating the XYSeries data object from the input Integer array values.
-				 */
-				for (int i = 0; i < dataValues.length; i++) {
-					data.add(i, dataValues[i]);
+		for (int i = 0; i < temps.size(); i++) {
+			int count = 1;
+			for (int j = i + 1; j < temps.size(); j++) {
+				if (temps.get(i) == temps.get(j)) {
+					count++;
+					// To avoid counting same element again
+					fr[j] = visited;
 				}
-
-				// add the series to the dataset
-				dataset.addSeries(data);
-
-				boolean legend = false; // do not visualize a legend
-				boolean tooltips = false; // do not visualize tooltips
-				boolean urls = false; // do not visualize urls
-
-				// Declare and initialize a createXYLineChart JFreeChart
-				JFreeChart chart = ChartFactory.createXYLineChart("Chart title", "x_axis title", "y_axis_title",
-						dataset, PlotOrientation.VERTICAL, legend, tooltips, urls);
-
-				/*
-				 * Initialize a frame for visualizing the chart and attach the previously
-				 * created chart.
-				 */
-				ChartFrame frame = new ChartFrame("First", chart);
-				frame.pack();
-				// makes the previously created frame visible
-				frame.setVisible(true);
 			}
-
-			
+			if (fr[i] != visited)
+				fr[i] = count;
 		}
 	}
+
+	/***
+	 * Receives a single dimension Integer array. From this array the dataset that
+	 * will be used for the visualization is generated. Finally, The chart is
+	 * generated with the use of the aforementioned dataset and then presented in
+	 * the screen.
+	 * 
+	 * @param dataValues Single dimension integer array
+	 */
+	public void generateChart(int[] dataValues) {
+		/*
+		 * The XYSeriesCollection object is a set XYSeries series (dataset) that can be
+		 * visualized in the same chart
+		 */
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		/*
+		 * The XYSeries that are loaded in the dataset. There might be many series in
+		 * one dataset.
+		 */
+		XYSeries data = new XYSeries("random values");
+
+		/*
+		 * Populating the XYSeries data object from the input Integer array values.
+		 */
+		for (int i = 0; i < dataValues.length; i++) {
+			data.add(i, dataValues[i]);
+		}
+
+		// add the series to the dataset
+		dataset.addSeries(data);
+
+		boolean legend = false; // do not visualize a legend
+		boolean tooltips = false; // do not visualize tooltips
+		boolean urls = false; // do not visualize urls
+
+		// Declare and initialize a createXYLineChart JFreeChart
+		JFreeChart chart = ChartFactory.createXYLineChart("Chart title", "x_axis title", "y_axis_title", dataset,
+				PlotOrientation.VERTICAL, legend, tooltips, urls);
+
+		/*
+		 * Initialize a frame for visualizing the chart and attach the previously
+		 * created chart.
+		 */
+		ChartFrame frame = new ChartFrame("First", chart);
+		frame.pack();
+		// makes the previously created frame visible
+		frame.setVisible(true);
+	}
+
 }
