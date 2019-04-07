2da7d1a3203dc318d425871dfa07b4fa7803b5a5
Stavros Karagiannidis <32717234+Stavros1997@users.noreply.github.com>
2019-04-07 03:14:11 +0300

    adding author strategy

--- code_analyzer/src/main/java/code_analyzer/Strategy.java
+++ code_analyzer/src/main/java/code_analyzer/Strategy.java
@@ -1,5 +1,9 @@
 package code_analyzer;
-
+/***
+ * 
+ * @author StavrosKar The purpose of this class is to be implemented by regex and string analyzers.
+ *
+ */
 public interface Strategy {
 	public int calculatemetrics(String[] file);
 	
